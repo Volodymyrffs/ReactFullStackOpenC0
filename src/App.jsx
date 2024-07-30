a704c64c16903fc10f3be21eb1654f64ca8f2c7d
Volodymyrffswtff <ffswtff@gmail.com>
2024-07-30 10:47:12 +0300

    total

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 const App = () => {
     const course = 'Half Stack application development'
     const part1 = 'Fundamentals of React'
+
+
     const exercises1 = 10
     const part2 = 'Using props to pass data'
     const exercises2 = 7
     const part3 = 'State of a component'
     const exercises3 = 14
     const Part1=(props)=> {
+        console.log(props.part1.name, props.part1.exercises)
          return (
 
              <p>{props.part1}  {props.exercises1}</p>
@@ -44,13 +47,19 @@ const App = () => {
             </>
         )
     }
-    const Total=()=>{}
+    const Total=(props)=>{
+        const total=props.exercises1+props.exercises2+props.exercises3
+        return (
+            <p>{total}</p>
+        )
+    }
+
 
     return (
         <div>
             <Header course={course} />
             <Content/>
-            <Total/>
+            <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
 
             {/*<p>*/}
             {/*    {part1} {exercises1}*/}
